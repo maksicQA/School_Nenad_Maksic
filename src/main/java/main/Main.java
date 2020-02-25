0cd41c80b570ca2d7b13b957ba094237674c1b26
Nenad <maksicnenad97@gmail.com>
2020-02-25 18:56:27 +0100

    Napravljena klasa Studen

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,17 @@
+
+package main;
+
+/**
+ *
+ * @author qa
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+    }
+    
+}
---
+++ src/main/java/student/Student.java
@@ -0,0 +1,103 @@
+
+package student;
+
+/**
+ *
+ * @author qa
+ */
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int yearOfBirth;
+    
+    
+    
+    
+    
+    public String getFirstName() {
+        return this.firstName;
+    }
+    
+    public void setFirstName(String customFirstName) {
+        this.firstName = customFirstName;
+    }
+    
+    public String getLastName() {
+        return this.lastName;
+    }
+    
+    public void setLastName(String customLastName) {
+        this.firstName = customLastName;
+    }
+    
+     public int getYearOfBirth() {
+        return this.yearOfBirth;
+    }
+    
+    public void setYearOfBirth(int customYearOfBirth) {
+        this.yearOfBirth = customYearOfBirth;
+    }
+    
+    
+    public Student() {
+        
+    } 
+    
+    
+    public Student(String customFirstName, String customLastName, int customYearOfBirth) {
+        this.firstName = customFirstName;
+        this.lastName = customLastName;
+        this.yearOfBirth = customYearOfBirth;
+    }
+    
+    
+    public void info() {
+        System.out.println("First name: " + this.getFirstName());
+        System.out.println("Last name: " + this.getLastName());
+        System.out.println("Year of Birth: " + this.getYearOfBirth());
+    }
+    
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
+
